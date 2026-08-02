89bba67e1d5461b36f7172235d7454117cdd5d16
kevin <kevinheins@live.de>
2026-08-02 15:11:55 +0200

    pokeObject angepasst: Änderungen: Types, Name - Hinzugefügt: Cry, Flavor Text(Fetch für FT erstellt) | Beispielfunktion anzeigeUndTimeout separat erstellt

--- fetch.js
+++ fetch.js
@@ -1,10 +1,9 @@
-import { createCard } from "./pokeCard";
-
 const input = document.querySelector("form");
 const pokeImg = document.querySelector("#poke-img");
 const btn = document.querySelector("#saveBtn");
 const mainPokemon = document.querySelector("#main-pokemon");
 const url = "https://pokeapi.co/api/v2/pokemon";
+const urlFlavor = "https://pokeapi.co/api/v2/pokemon-species/";
 
 //pokeObject
 const pokeObject = {
@@ -18,14 +17,16 @@ const pokeObject = {
     Defense: "???",
     Speed: "???",
   },
+  cry: "",
+  flavorText: "???",
   pokeNote: "Write a personal Note ✏️",
 };
 
-input.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const pokeName = formData.get("q")?.toLocaleLowerCase();
+// btn logic
+// Anzeige von Pokemon und blinkender Button mit Delay bis Anzeige
+function anzeigeUndTimeout() {
   btn.disabled = true;
+  btn.classList.remove("bg-white");
   btn.classList.add(
     "transition-all",
     "duration-700",
@@ -35,54 +36,88 @@ input.addEventListener("submit", (event) => {
     "shadow-lg",
     "shadow-red-300",
   );
-  btn.classList.remove("bg-white");
 
   setTimeout(() => {
-    fetch(`${url}/${pokeName}`)
-      .then((res) => {
-        if (!res.ok) throw new Error(`${pokeName} wasn't found`);
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        pokeImg.src = data.sprites.front_default;
-        pokeImg.classList.remove("hidden");
-        mainPokemon.textContent = `Name: ${data.stats[0].base_stat}
-        ID: ${data.id}`;
+    btn.classList.remove(
+      "bg-red-400",
+      "animate-pulse",
+      "shadow-lg",
+      "shadow-red-300",
+    );
+    btn.classList.add("bg-white");
+    btn.disabled = false;
 
-        // object logic
-        pokeObject.id = data.id;
-        pokeObject.pokeName = data.species.name;
-        pokeObject.pokeType = data.types[0].type.name;
-        pokeObject.pokeImg = data.sprites.front_default;
-        pokeObject.pokeStats.HP = data.stats[0].base_stat;
-        pokeObject.pokeStats.Attack = data.stats[1].base_stat;
-        pokeObject.pokeStats.Defense = data.stats[2].base_stat;
-        pokeObject.pokeStats.Speed = data.stats[5].base_stat;
-        console.log(pokeObject);
+    pokeImg.src = pokeObject.pokeImg;
+    pokeImg.classList.remove("hidden");
+    mainPokemon.textContent = `Name: ${pokeObject.pokeName}
+    ID: ${pokeObject.id}
+    Type: ${pokeObject.pokeType.join("/")}
+    Dex Entry: ${pokeObject.flavorText}`;
+    const pokeCry = new Audio(pokeObject.cry);
+    pokeCry.play();
 
-        //btn logic
-        btn.classList.remove(
-          "bg-red-400",
-          "animate-pulse",
-          "shadow-lg",
-          "shadow-red-300",
-        );
-        btn.classList.add("bg-white");
-        btn.disabled = false;
-      })
-      .catch((error) => {
-        console.error("Oh oh. ", error);
-        mainPokemon.textContent = "No Pokemon found";
-        btn.classList.remove(
-          "bg-red-400",
-          "animate-pulse",
-          "shadow-lg",
-          "shadow-red-300",
-        );
-        btn.classList.add("bg-white");
-        btn.disabled = false;
-      });
+    btn.classList.add("bg-white");
+    btn.disabled = false;
     input.reset();
   }, 2000);
+}
+
+input.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const pokeName = formData.get("q")?.toLocaleLowerCase();
+
+  // 1/2 Fetch - alles außer Flavor Text
+  fetch(`${url}/${pokeName}`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`${pokeName} wasn't found`);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      // object logic
+      pokeObject.id = data.id;
+      pokeObject.pokeName =
+        data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1);
+      const typeList = Object.keys(data.types);
+      for (const el of typeList) {
+        typeList[el] = data.types[el].type.name;
+        typeList[el] =
+          typeList[el].charAt(0).toUpperCase() + typeList[el].slice(1);
+      }
+      pokeObject.pokeType = typeList;
+      pokeObject.pokeImg = data.sprites.front_default;
+      pokeObject.pokeStats.HP = data.stats[0].base_stat;
+      pokeObject.pokeStats.Attack = data.stats[1].base_stat;
+      pokeObject.pokeStats.Defense = data.stats[2].base_stat;
+      pokeObject.pokeStats.Speed = data.stats[5].base_stat;
+      pokeObject.cry = data.cries.legacy;
+      console.log(pokeObject);
+    })
+    .then(() => {
+      // 2/2 Fetch - Flavor Text
+      fetch(`${urlFlavor}/${pokeObject.id}`)
+        .then((res) => {
+          if (!res.ok) throw new Error(`${pokeName} wasn't found`);
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          let flavorTextObj = data.flavor_text_entries.find(
+            (entry) => entry.language.name === "en",
+          );
+          pokeObject.flavorText = flavorTextObj
+            ? flavorTextObj.flavor_text.replace(/[\n\f]/g, " ")
+            : "No entry.";
+          console.log(pokeObject.flavorText);
+        })
+        .then(() => {
+          anzeigeUndTimeout();
+        });
+    })
+    .catch((error) => {
+      console.error("Oh oh. ", error);
+      mainPokemon.textContent = "No Pokemon found";
+    });
 });
